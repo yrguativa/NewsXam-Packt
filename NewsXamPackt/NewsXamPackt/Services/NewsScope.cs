98da1f8464b99fe64fb5e9810fd135a350e9ab2c
Yilmer Guativa <ingeniero.master5@interrapidisimo.com>
2020-12-07 17:41:22 -0500

    set up for services

---
+++ NewsXamPackt/NewsXamPackt/Services/NewsScope.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsXamPackt.Services
+{
+    public enum NewsScope
+    {
+        Headlines,
+        Local,
+        Global
+    }
+
+}
---
+++ NewsXamPackt/NewsXamPackt/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsXamPackt
+{
+    public static  class Settings
+    {
+        public static string NewsApiKey
+        {
+            get
+            {
+                return "<Your APIKey here>";
+            }
+        }
+
+    }
+}
